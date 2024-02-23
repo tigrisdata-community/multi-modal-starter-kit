e07ff50b22df81131dbdb9229ef136a8980a7ed9
Adil Ansari <adil@tigrisdata.com>
2024-02-23 12:43:39 -0800

    added content loading from storage

--- src/app/api/files/route.ts
+++ src/app/api/files/route.ts
@@ -1,3 +1,10 @@
+import { ListObjectsV2Command, S3Client } from '@aws-sdk/client-s3'
+
+const client = new S3Client();
+
 export async function GET() {
+  const listObjectsV2Command = new ListObjectsV2Command({Bucket: process.env.BUCKET_NAME});
+  const resp = await client.send(listObjectsV2Command);
+  console.log(resp.Contents);
   return Response.json([{"name": "file1", "id": "f_123"}])
 }
