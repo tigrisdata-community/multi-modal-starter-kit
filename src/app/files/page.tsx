c00048b47c1667a020cc8e45077b3af83f409419
Adil Ansari <adil@tigrisdata.com>
2024-02-25 14:15:37 -0800

    styling the list

--- src/app/files/page.tsx
+++ src/app/files/page.tsx
@@ -1,19 +1,25 @@
 import { FilesResponse } from '@/app/api/files/route'
 import { headers } from 'next/headers'
+import styles from './page.module.css'
 
-export default async function Page() {
-  const host = headers().get("host");
+export default async function Page () {
+  const host = headers().get('host')
   const res = await fetch(`http://${host}/api/files/`, {
-    method: "GET",
+    method: 'GET',
     cache: 'no-store'
-  });
-  const files: FilesResponse = await res.json();
-  console.log(files);
+  })
+  const files: FilesResponse = await res.json()
+  console.log(files)
   return (
-    <ul>
-      {files.map((f, index) => (
-        <li key={f.key}>{index+1}. {f.displayName}</li>
-      ))}
-    </ul>
+    <body className={styles.body}>
+    <div className={styles.list}>
+      <h2>Available videos</h2>
+      <ul>
+        {files.map((f, index) => (
+          <li key={f.key}><span>{index + 1}. {f.displayName}</span></li>
+        ))}
+      </ul>
+    </div>
+    </body>
   )
 }
