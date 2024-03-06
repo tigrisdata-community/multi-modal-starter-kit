acdc0313d46d10cc73a31e5a8e8fee651e06230d
Adil Ansari <adil@tigrisdata.com>
2024-03-05 16:28:50 -0800

    publishing video from multiple sources

--- webapp/src/app/files/[slug]/page.tsx
+++ webapp/src/app/files/[slug]/page.tsx
@@ -1,9 +1,14 @@
 export default async function Page({ params }: { params: { slug: string } }) {
   return (
     <body>
-      <h3>Now playing video: {params.slug}</h3>
+      <h3>Video from Tigris: {params.slug}</h3>
       <video id="vid1" width="640" height="480" controls preload="auto" data-setup="{}">
-        <source src="https://tigris-mm-starter-kit-test.fly.storage.tigris.dev/city_lights_sample.mp4" type="video/mp4" />
+        <source src="https://tigris-mm-starter-kit-test.fly.storage.tigris.dev/city_lights_sample.mp4" type="video/mp4"/>
+      </video>
+
+    <h3>Video with Content-Type video/mp4</h3>
+    <video id="vid1" width="640" height="480" controls preload="auto" data-setup="{}">
+        <source src="https://file-examples.com/storage/fe7b7e0dc465e22bc9e6da8/2017/04/file_example_MP4_640_3MG.mp4" type="video/mp4" />
       </video>
     </body>
   )
