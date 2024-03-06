2ae7aa4acf678a096d11fa64b0dee05d2fd3fd21
Adil Ansari <adil@tigrisdata.com>
2024-03-06 08:31:56 -0800

    removing the extra file

--- webapp/src/app/files/play/page.tsx
+++ webapp/src/app/files/play/page.tsx
@@ -8,11 +8,6 @@ export default async function Page({searchParams}: {searchParams: {
       <video id="vid1" width="640" height="480" controls preload="auto" data-setup="{}">
         <source src={`${videoUrl}`} type="video/mp4" />
       </video>
-
-      <h3>Random file from internet with Content-Type video/mp4</h3>
-      <video id="vid1" width="640" height="480" controls preload="auto" data-setup="{}">
-        <source src="https://file-examples.com/storage/fe7b7e0dc465e22bc9e6da8/2017/04/file_example_MP4_640_3MG.mp4" type="video/mp4" />
-      </video>
     </body>
   )
 }
