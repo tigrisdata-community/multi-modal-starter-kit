0a4a7935ebe1d9b6506d276caf212d35ba81cd17
Adil Ansari <adil@tigrisdata.com>
2024-03-07 10:13:07 -0800

    testing streams

--- webapp/src/app/files/play/page.tsx
+++ webapp/src/app/files/play/page.tsx
@@ -1,13 +1,52 @@
-export default async function Page({searchParams}: {searchParams: {
-  name: string;
-}}) {
+'use client'
+import { useEffect, useState } from "react";
+
+export default function Page({ searchParams }: {
+  searchParams: {
+    name: string;
+  }
+}) {
+
+  const host = 'localhost:3000'
   const videoUrl: string = `https://${process.env.BUCKET_NAME}.fly.storage.tigris.dev/${searchParams.name}`
+  const [narration, setNarration] = useState("");
+  const [eachNar, setEachNar] = useState("");
+
+  useEffect(() => {
+    const res = fetch(`http://${host}/api/describe/`, {
+      method: 'POST',
+      body: JSON.stringify({
+        'url': videoUrl
+      })
+    }).then(async (response) => {
+      const result = await response.json();
+      setNarration(result.data);
+    });
+  }, [videoUrl])
+
+
+  useEffect(() => {
+    if (narration != '') {
+      let incre = 0;
+      const timeoutId = setInterval(() => {
+        setEachNar(narration[incre]);
+        incre++;
+        if (incre >= narration.length) {
+          clearTimeout(timeoutId)
+        }
+      }, 1000)
+      return () => clearTimeout(timeoutId);
+    }
+  }, [narration])
+
   return (
     <body>
       <h3>Playing video from Tigris:</h3>
       <video id="vid1" width="640" height="480" controls preload="auto" data-setup="{}">
         <source src={`${videoUrl}`} type="video/mp4" />
       </video>
+      <h3>Narration using GPT 4 vision:</h3>
+      <h4>{eachNar}</h4>
     </body>
   )
 }
