d1b72afa8217c151ed845e33d8b1171a09eee1f7
Adil Ansari <adil@tigrisdata.com>
2024-03-08 11:03:23 -0800

    functional backend

--- webapp/src/app/files/play/page.tsx
+++ webapp/src/app/files/play/page.tsx
@@ -15,23 +15,10 @@ export default function Page({ searchParams }: {
   const [eachNar, setEachNar] = useState("");
 
   useEffect(() => {
-    const res = fetch(`http://${host}/api/describe/`, {
-      method: 'POST',
-      body: JSON.stringify({
-        'url': videoUrl
-      })
-    }).then(async (response) => {
-      const result = await response.json();
-      setNarration(result.data);
-    });
-  }, [videoUrl])
-
-
-  useEffect(() => {
     if (narration != '') {
       let incre = 0;
       const timeoutId = setInterval(() => {
-        setEachNar(narration[incre]);
+        setEachNar(narration);
         incre++;
         if (incre >= narration.length) {
           clearTimeout(timeoutId)
@@ -45,48 +32,56 @@ export default function Page({ searchParams }: {
 
   const vidRef = useRef(null);
   const canRef = useRef(null);
-  
+
   const handlePlayVideo = () => {
-    if(vidRef.current != null){
+    if (vidRef.current != null) {
       vidRef.current.play();
     }
   }
-  
 
-  function captureFrame() {     
 
-    if(canRef.current && vidRef.current){
+  function captureFrame() {
+
+    if (canRef.current && vidRef.current) {
       const context = canRef.current.getContext('2d');
       context.drawImage(vidRef.current, 0, 0, 110, 120);
 
       const dataURL = canRef.current.toDataURL('image/jpeg', 0.5);
 
       console.log(dataURL);
+
+      fetch(`http://${host}/api/describe/`, {
+        method: 'POST',
+        body: JSON.stringify({
+          'frame': dataURL
+        })
+      }).then(async (response) => {
+        const result = await response.text();
+        setNarration(result);
+      });
     }
-
-}
-
+  }
 
   return (
     <>
       <h3>Playing video from Tigris:</h3>
-    
+  
     <p>{videoUrl}</p>
  
-      <video ref={vidRef} width="640" height="480"  controls preload="auto" data-setup="{}">
+      <video ref={vidRef} crossOrigin="" width="640" height="480" controls preload="auto" data-setup="{}">
         <source src={videoUrl} type="video/mp4" />
       </video>
       
       <button onClick={handlePlayVideo}>
-            Play
-          </button>
+        Play
+      </button>
       <button onClick={captureFrame}>
-            Capture
-        </button>
+        Capture
+      </button>
       <h3>Narration using GPT 4 vision:</h3>
       <h4>{eachNar}</h4>
-      
-      <canvas ref={canRef} width="200" height="200" style={{overflow:'auto'}}></canvas>
-      </>
+
+      {/* <canvas ref={canRef} width="200" height="200" style={{ overflow: 'auto' }}></canvas> */}
+    </>
   )
 }
