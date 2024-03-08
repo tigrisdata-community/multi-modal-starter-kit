a71ff4fbe32558a88bebf8032a5c4bfaf328ec14
Taha Khan <tkhan@tigrisdata.com>
2024-03-08 09:17:26 -0800

    screen capture and base64 test

--- webapp/src/app/api/describe/route.ts
+++ webapp/src/app/api/describe/route.ts
@@ -1,49 +1,9 @@
+import { describeImage } from "@/app/utils";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const videoUrl = (await req.json())["url"];
-  const outFilePrefix = "./frame";
-
-
-  // const iter = makeIterator();
-  // const stream = iteratorToStream(iter);
-  return Response.json({
-    "data": ["one-t", "two-t", "three-t"]
-  });
+  const videoUrl = (await req.json())["frame"];
+  const aiResponse = await describeImage(videoUrl);
+  console.log("querying openai for narration")
+  return new Response(aiResponse.content);
 }
-
-function iteratorToStream(iterator: any) {
-  return new ReadableStream({
-    async pull(controller) {
-      const { value, done } = await iterator.next()
-
-      if (done) {
-        controller.close()
-      } else {
-        controller.enqueue(value)
-      }
-    },
-  })
-}
-
-function sleep(time: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, time)
-  })
-}
-
-const encoder = new TextEncoder()
-
-async function* makeIterator() {
-  yield encoder.encode('<p>One</p>')
-  await sleep(2000)
-  yield encoder.encode('<p>Two</p>')
-  await sleep(2000)
-  yield encoder.encode('<p>Three</p>')
-}
-
-// next steps
-// 1. Create a iterator stream from next js 
-// example and be able to update text on the front end from it
-// 2. Once text is updating look into the logic of reading 
-// video frames from URL and return frame by frame response to iterator
--- webapp/src/app/files/play/page.tsx
+++ webapp/src/app/files/play/page.tsx
@@ -1,35 +1,21 @@
 'use client'
-import { useEffect, useState } from "react";
+
+import { useEffect, useRef, useState } from "react";
 
 export default function Page({ searchParams }: {
   searchParams: {
     name: string;
   }
 }) {
-
-  const host = 'localhost:3000'
-  const videoUrl: string = `https://${process.env.BUCKET_NAME}.fly.storage.tigris.dev/${searchParams.name}`
+  const videoUrl: string = `https://${process.env.NEXT_PUBLIC_BUCKET_NAME}.fly.storage.tigris.dev/${searchParams.name}`
   const [narration, setNarration] = useState("");
   const [eachNar, setEachNar] = useState("");
-
-  useEffect(() => {
-    const res = fetch(`http://${host}/api/describe/`, {
-      method: 'POST',
-      body: JSON.stringify({
-        'url': videoUrl
-      })
-    }).then(async (response) => {
-      const result = await response.json();
-      setNarration(result.data);
-    });
-  }, [videoUrl])
-
 
   useEffect(() => {
     if (narration != '') {
       let incre = 0;
       const timeoutId = setInterval(() => {
-        setEachNar(narration[incre]);
+        setEachNar(narration);
         incre++;
         if (incre >= narration.length) {
           clearTimeout(timeoutId)
@@ -39,14 +25,48 @@ export default function Page({ searchParams }: {
     }
   }, [narration])
 
+  const vidRef = useRef(null);
+  const canRef = useRef(null);
+
+  const handlePlayVideo = () => {
+    if (vidRef.current != null) {
+      vidRef.current.play();
+    }
+  }
+
+  function captureFrame() {
+    if (canRef.current && vidRef.current) {
+      const context = canRef.current.getContext('2d');
+      context.drawImage(vidRef.current, 0, 0, 110, 120);
+      const dataURL = canRef.current.toDataURL('image/jpeg', 0.5);
+      console.log(dataURL);
+      fetch(`/api/describe/`, {
+        method: 'POST',
+        body: JSON.stringify({
+          'frame': dataURL
+        })
+      }).then(async (response) => {
+        const result = await response.text();
+        setNarration(result);
+      });
+    }
+  }
+
   return (
-    <body>
+    <>
       <h3>Playing video from Tigris:</h3>
-      <video id="vid1" width="640" height="480" controls preload="auto" data-setup="{}">
-        <source src={`${videoUrl}`} type="video/mp4" />
+      <video ref={vidRef} crossOrigin="" width="640" height="480" controls preload="auto" data-setup="{}">
+        <source src={videoUrl} type="video/mp4" />
       </video>
+      <button onClick={handlePlayVideo}>
+        Play
+      </button>
+      <button onClick={captureFrame}>
+        Capture
+      </button>
       <h3>Narration using GPT 4 vision:</h3>
       <h4>{eachNar}</h4>
-    </body>
+      <canvas ref={canRef} width="640" height="480" style={{ overflow: 'auto' }}></canvas>
+    </>
   )
 }
